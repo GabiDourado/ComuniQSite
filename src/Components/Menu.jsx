54b8dc15af580db2427e2bd697e29b996b730c21
majumxrs <mariaescola06@gmail.com>
2024-09-17 13:45:02 -0300

    so uma partefeita

---
+++ src/Components/Menu.jsx
@@ -0,0 +1,12 @@
+import logo from "../img/LogoComuniQ.jpeg"
+import "../Css/menu.css"
+function Menu() {
+    return (
+        <>
+            <div className="cima">
+                <img src={logo} alt="Logo" className="logo" />
+            </div>
+        </>
+    )
+}
+export default Menu;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../Css/home.css";
+function Home() {
+    return (
+        <>
+            <div className='DivTexto1'>
+                <a className='ComuniQ1'>CominiQ</a><a className='escrita1'>, o App Que Une Pessoas E Investe Em Uma Boa Vivência</a>
+            </div>
+            <div className='DivTexto2'>
+                <a className='escrita2' >Bem Vindo</a>
+            </div>
+        </>
+    )
+}
+
+export default Home;
