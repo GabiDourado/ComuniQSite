550ef3e46cfe2bcb6ec0607e330153352a580764
Rafaolez <raafaaeel.camiloo@gmail.com>
2024-09-24 15:10:27 -0300

    Finalizado ufa

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import GooglePlay from "../img/GooglePlay.png";
+import IosPlay from "../img/IosPlay.png";
 import comuniQ3app from "../img/ComuniQ3app.png"
 import "../Css/home.css";
 function Home() {
@@ -7,22 +8,21 @@ function Home() {
         <div className='Imagem'>
 
             <div className='DivTexto1'>
-                <a className='ComuniQ1'>CominiQ</a><a className='escrita1'>, o App Que Une Pessoas E Investe Em Uma Boa Vivência</a>
+                <a className='ComuniQ1'>CominiQ</a><a className='escrita1'> o App Que Une Pessoas E Investe Em Uma Boa Vivência</a>
             </div>
-            <div className='DivTexto2'>
-                <a className='escrita2' >Bem Vindo</a>
-            </div>
+            {/* <div className="DivTextoB">
+                <text className="BTTTAQUI">Baixe aqui!!!</text>
+            </div> */}
             <div className="Divbtn">
-                <button className='btn' >Baixe Já!</button>
+                <a className='btn' href="https://github.com/majumxrs/ComuniQApp" ><img src={GooglePlay} alt="Logo" className="GooglePlay" /></a>
+                <a className='btn' href="https://github.com/majumxrs/ComuniQApp" ><img src={IosPlay} alt="Logo" className="IosPlay" /></a>
+                {/* <button className='btn' ><img src={GooglePlay} alt="Logo" className="GooglePlay" /></button> */}
             </div>
             <div className='DivTexto3'>
                 <a className='escrita3'>Fique Por Dentro De Tudo Que Acontece Na Sua Comunidade!</a>
             </div>
             <div className='DivImgApp'>
                 <img src={comuniQ3app} alt="layalt do App" className="app" />
-            </div>
-            <div className='DivImg'>
-                <img src={GooglePlay} alt="Logo" className="GooglePlay" />
             </div>
         </div>
 
